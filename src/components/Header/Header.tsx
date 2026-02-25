648349a8983ff505c49392e1aaa6330212d7659f
ronlnn <artking01kg@gmail.com>
2026-02-25 18:57:44 +0300

    feat: refactored Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import styles from './Header.module.scss';
 
 import { Button, Logo } from '../../ui';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
     <header className={styles.mainHeader}>
-      <Logo className={styles.headerLogo} />
+      <Link to={'/'}>
+        <Logo className={styles.headerLogo} />
+      </Link>
       <Button variant="light">Связаться с нами</Button>
     </header>
   );
--- src/components/NewsCard/NewsCard.tsx
+++ src/components/NewsCard/NewsCard.tsx
@@ -3,28 +3,31 @@ import styles from './NewsCard.module.scss';
 import type { NewsCardProps } from './NewsSection.types';
 import { Heading, Text } from '../../ui';
 import { useMediaQuery } from '../../utils/useMediaQuery';
+import { Link } from 'react-router-dom';
 
 const NewsCard: React.FC<NewsCardProps> = ({ item }) => {
   const isMobile = useMediaQuery('(max-width: 480px)');
   return (
     <article className={styles.card}>
-      <img
-        src={item.image}
-        alt={item.title}
-        className={styles.cardImage}
-        loading="lazy"
-      />
-      <div className={styles.cardContent}>
-        <Heading tag={isMobile ? 'h3' : 'h2'} className={styles.cardTitle}>
-          {item.title}
-        </Heading>
-        <Text tag={'p'} className={styles.cardDescription}>
-          {item.description}
-        </Text>
-        <Text tag={'p'} className={styles.cardDate}>
-          {item.date}
-        </Text>
-      </div>
+      <Link to={`news/${item.id}`} className={styles.cardLink}>
+        <img
+          src={item.image}
+          alt={item.title}
+          className={styles.cardImage}
+          loading="lazy"
+        />
+        <div className={styles.cardContent}>
+          <Heading tag={isMobile ? 'h3' : 'h2'} className={styles.cardTitle}>
+            {item.title}
+          </Heading>
+          <Text tag={'p'} className={styles.cardDescription}>
+            {item.description}
+          </Text>
+          <Text tag={'p'} className={styles.cardDate}>
+            {item.date}
+          </Text>
+        </div>
+      </Link>
     </article>
   );
 };
